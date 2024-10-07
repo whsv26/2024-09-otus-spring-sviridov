e8bee8880a06c81c9c5f4d21a277efbe75fa09db
Свиридов Александр <sviridovav@m2.ru>
2024-10-07 16:19:51 +0300

    HW01: Remove excessive list to array conversion

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -5,8 +5,7 @@ import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 
-import java.util.function.Function;
-import java.util.stream.IntStream;
+import java.util.List;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
@@ -19,7 +18,7 @@ public class TestServiceImpl implements TestService {
     @Override
     public void executeTest() {
         var header = writeHeader();
-        var questions = writeQuestions(questionsDao.findAll().toArray(Question[]::new));
+        var questions = writeQuestions(questionsDao.findAll());
         Stream.concat(header, questions).forEach(ioService::printLine);
     }
 
@@ -27,20 +26,28 @@ public class TestServiceImpl implements TestService {
         return Stream.of("", "Please answer the questions below", "");
     }
 
-    private static Stream<String> writeQuestions(Question[] questions) {
-        return IntStream.range(0, questions.length)
-            .mapToObj(i -> writeQuestion(questions[i], i + 1))
-            .flatMap(Function.identity());
+    private static Stream<String> writeQuestions(List<Question> questions) {
+        Stream<String> lines = Stream.empty();
+        int nth = 1;
+
+        for (var question : questions) {
+            lines = Stream.concat(lines, writeQuestion(question, nth++));
+            lines = Stream.concat(lines, Stream.of(""));
+        }
+
+        return lines;
     }
 
     private static Stream<String> writeQuestion(Question question, int nth) {
         var questionText = Stream.of("%d) %s".formatted(nth, question.text()));
-        var questionAnswers = question.answers().stream()
+        var questionAnswers = writeAnswers(question.answers());
+        return Stream.concat(questionText, questionAnswers);
+    }
+
+    private static Stream<String> writeAnswers(List<Answer> answers) {
+        return answers.stream()
             .map(Answer::text)
             .map("- %s"::formatted);
-
-        return Stream.of(questionText, questionAnswers, Stream.of(""))
-            .flatMap(Function.identity());
     }
 
 }

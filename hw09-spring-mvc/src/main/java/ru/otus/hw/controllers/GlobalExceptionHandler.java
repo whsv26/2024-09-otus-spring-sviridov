492ec816badddec7522ee571bcc161f284b55e3d
whsv26 <whsv26@gmail.com>
2025-01-04 02:47:49 +0300

    HW09: Impl

---
+++ hw09-spring-mvc/src/main/java/ru/otus/hw/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package ru.otus.hw.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+
+@RequiredArgsConstructor
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ModelAndView handeNotFoundException(EntityNotFoundException ex) {
+        var errorText = ex.getMessage();
+        return new ModelAndView("customError", "errorText", errorText);
+    }
+
+}

680010d63b021b973f0788fc7e06467b95379625
whsv26 <whsv26@gmail.com>
2025-03-10 22:13:52 +0300

    HW19: Impl

--- hw19-project-work/api-gateway/src/main/java/ru/demo/config/ApiConfig.java
+++ hw19-project-work/api-gateway/src/main/java/ru/demo/config/ApiConfig.java
@@ -2,7 +2,10 @@ package ru.demo.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,21 +17,35 @@ import ru.demo.controller.RequestIdFilter;
 public class ApiConfig {
 
     @Bean
-    public RequestIdFilter xrequestFilter() {
+    public RequestIdFilter requestIdFilter() {
         return new RequestIdFilter();
     }
 
     @Bean
     RouteLocator gateway(
-            RouteLocatorBuilder rlb, ApplConfigProperties applConfigProperties, RequestIdFilter xrequestFilter) {
-        var routesBuilder = rlb.routes();
+        RouteLocatorBuilder routeLocatorBuilder,
+        ApplConfigProperties applConfigProperties,
+        RequestIdFilter filter
+    ) {
+        var routesBuilder = routeLocatorBuilder.routes();
         for (var route : applConfigProperties.getApiRoutes()) {
-            routesBuilder.route(route.id(), routeSpec -> routeSpec
-                    .path(String.format("/%s/**", route.from()))
-                    .filters(fs -> fs.filters(xrequestFilter)
-                            .rewritePath(String.format("/%s/(?<segment>.*)", route.from()), "/${segment}"))
-                    .uri(String.format("%s/@", route.to())));
+            routesBuilder.route(
+                route.id(),
+                routeSpec -> buildRoute(route, routeSpec, filter)
+            );
         }
         return routesBuilder.build();
+    }
+
+    private static Buildable<Route> buildRoute(
+        ApiRoute route,
+        PredicateSpec routeSpec,
+        RequestIdFilter requestIdFilter
+    ) {
+        return routeSpec
+            .path(String.format("/%s/**", route.from()))
+            .filters(filterSpec -> filterSpec.filters(requestIdFilter)
+            .rewritePath(String.format("/%s/(?<segment>.*)", route.from()), "/${segment}"))
+            .uri(String.format("%s/@", route.to()));
     }
 }
